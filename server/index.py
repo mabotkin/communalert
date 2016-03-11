fac8deafa5d4dd39bd13314a812056cd79e0cf74
Alex Wang <alex3763@gmail.com>
2016-03-11 15:11:25 -0500

    basic get and post methods

--- server/index.py
+++ server/index.py
@@ -1,10 +1,23 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-	return "Hello, World!"
+@app.route("/GET", methods=['GET'])
+def get():
+	latitude = float(request.args['latitude'])
+	longitude = float(request.args['longitude'])
+	# make sql request
+	# sort with lambda = DPA
+	# send back json of most relevant
+	# json.dumps("stuff")
+
+@app.route("/POST",methods=['POST']
+def post():
+	latitude = float(request.form['latitude'])
+	longitude = float(request.form['longitude'])
+	userid = float(request.form['user_id'])
+	reporttype = request.form(['type'])
 
 if __name__ == "__main__":
 	app.run()
