cf6e4cc4a6db1fe026f3c3a53bdeeb6b51301100
Alex Wang <alex3763@gmail.com>
2016-03-12 17:07:16 -0500

    post working + fetches first entry

--- server/index.py
+++ server/index.py
@@ -1,33 +1,45 @@
-from flask import Flask
+from flask import Flask, request
 import json
 import MySQLdb # pip install MySQL-python
 
-ADMINUSERNAME = ""
-ADMINPASSWORD = ""
+USERNAME = "communalert"
+PASSWORD = "communalert"
 
 app = Flask(__name__)
+app.debug = True
+
+@app.route("/")
+def root():
+	return "xD"
 
 @app.route("/GET", methods=['GET'])
 def get():
 	latitude = float(request.args['latitude'])
 	longitude = float(request.args['longitude'])
 	# make sql request
-	db = MySQLdb.connect("localhost", ADMINUSERNAME , ADMINPASSWORD , "communalert");
+	db = MySQLdb.connect("localhost", USERNAME , PASSWORD , "communalert");
 	cursor = db.cursor()
-	cursor.execute("SELECT * FROM ")
+	cursor.execute("SELECT * FROM reports WHERE report_time > NOW() - INTERVAL 60 MINUTE;")
 	data = cursor.fetchone()
 	# sort with lambda = DPA
 	# send back json of most relevant
 	# return json.dumps("stuff")
 	db.close()
+	return str(data)
 
-@app.route("/POST",methods=['POST']
+@app.route("/POST",methods=['POST'])
 def post():
 	latitude = float(request.form['latitude'])
 	longitude = float(request.form['longitude'])
 	userid = float(request.form['user_id'])
-	reporttype = request.form(['type'])
+	reporttype = request.form['type']
+	db = MySQLdb.connect("localhost", USERNAME , PASSWORD , "communalert");
+	cursor = db.cursor()
+	cursor.execute("INSERT INTO reports (user_id,latitude,longitude,report_type) VALUES (" + str(userid) + "," + str(latitude) + "," + str(longitude) + ",\"" + reporttype + "\");")
+	db.commit()
+	db.close()
+	return "success"
 	# sql insert into
 
 if __name__ == "__main__":
-	app.run()
+    app.run(host="127.0.0.1",port=3500)
--- server/setup.sql
+++ server/setup.sql
@@ -8,7 +8,7 @@ report_id int NOT NULL AUTO_INCREMENT,
 user_id int NOT NULL,
 latitude double NOT NULL,
 longitude double NOT NULL,
-report_time datetime NOT NULL,
+report_time timestamp DEFAULT CURRENT_TIMESTAMP,
 report_type varchar(32) NOT NULL,
 PRIMARY KEY(report_id)
 );
